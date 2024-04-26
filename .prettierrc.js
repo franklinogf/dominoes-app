2d7ed69175848104321e6bd1ee0f413dda353cc3
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-04-25 21:10:16 -0400

    installed eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  semi: false,
+  printWidth: 100,
+  jsxSingleQuote:true
+  
+};
