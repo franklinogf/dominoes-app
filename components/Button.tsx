33ecd5a50a8283df5dbd61df8aed8aec66dc5315
DarwinML2 <darwinmass.ml2@gmail.com>
2024-05-01 14:35:38 -0400

    color button and size

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,11 +11,18 @@ interface Props extends TouchableOpacityProps {
   label: string
   style?: StyleProp<ViewStyle>
   className?: string
+  size?: 'sm' | 'md' | 'lg' | 'xl'
   onPress?: () => void
 }
-export default function Button({ label, style, ...props }: Props) {
+
+export default function Button({ label, style, size, ...props }: Props) {
   return (
-    <Pressable activeOpacity={0.8} style={style} className='bg-primary-700 py-4 w-full' {...props}>
+    <Pressable
+      activeOpacity={0.8}
+      style={style}
+      className='bg-primary-400 py-4 w-full rounded-full'
+      {...props}
+    >
       <Text className='text-center font-semibold text-lg'>{label}</Text>
     </Pressable>
   )
