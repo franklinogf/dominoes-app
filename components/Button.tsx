875420a91500693e53d020bcb200c02a9a1ff28a
DarwinML2 <darwinmass.ml2@gmail.com>
2024-05-01 15:29:31 -0400

    added props sizes

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,19 +11,25 @@ interface Props extends TouchableOpacityProps {
   label: string
   style?: StyleProp<ViewStyle>
   className?: string
-  size?: 'sm' | 'md' | 'lg' | 'xl'
+  size?: keyof typeof sizeVariant
   onPress?: () => void
 }
+const sizeVariant = {
+  sm: 'px-1 py-1',
+  md: 'px-2 py-1',
+  lg: 'px-8',
+  xl: 'py-2',
+}
 
-export default function Button({ label, style, size, ...props }: Props) {
+export default function Button({ label, style, size = 'md', ...props }: Props) {
   return (
     <Pressable
       activeOpacity={0.8}
       style={style}
-      className='bg-primary-400 py-4 w-full rounded-full'
+      className={`bg-primary-400 rounded-lg ${sizeVariant[size]}`}
       {...props}
     >
-      <Text className='text-center font-semibold text-lg'>{label}</Text>
+      <Text className='text-center font-semibold text-lg text-white'>{label}</Text>
     </Pressable>
   )
 }
