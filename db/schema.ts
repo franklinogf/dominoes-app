e2225b9da4e3fd3a704adf222085b1b8eb19c259
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-05-14 12:45:27 -0400

    changed database

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,3 @@
-import { relations } from 'drizzle-orm'
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 const teamsEnum = ['team1', 'team2'] as const
 
@@ -7,24 +6,7 @@ export const games = sqliteTable('games', {
   winner: text('winner', { enum: teamsEnum }).notNull(),
   team1: text('team1').notNull(),
   team2: text('team2').notNull(),
+  score1: integer('score1').notNull(),
+  score2: integer('score2').notNull(),
   date: integer('date', { mode: 'timestamp' }).notNull(),
 })
-
-export const scores = sqliteTable('scores', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  gameId: integer('game_id')
-    .references(() => games.id)
-    .notNull(),
-  team: text('team', { enum: teamsEnum }).notNull(),
-  scores: text('scores', { mode: 'json' }),
-})
-
-export const gamesRelations = relations(games, ({ many }) => ({
-  scores: many(scores),
-}))
-export const scoresRelations = relations(scores, ({ one }) => ({
-  game: one(games, {
-    fields: [scores.gameId],
-    references: [games.id],
-  }),
-}))
---
+++ drizzle/0003_confused_leopardon.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `games` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`winner` text NOT NULL,
+	`team1` text NOT NULL,
+	`team2` text NOT NULL,
+	`score1` integer NOT NULL,
+	`score2` integer NOT NULL,
+	`date` integer NOT NULL
+);
