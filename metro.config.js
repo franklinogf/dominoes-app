a841cdfb4836289bae0b423c10379d0281dacbe7
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-06-06 17:11:49 -0400

    moved the css file inside app folder

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,4 @@ const { withNativeWind } = require("nativewind/metro")
 const config = getDefaultConfig(__dirname)
 
 config.resolver.sourceExts.push("sql")
-module.exports = withNativeWind(config, { input: "./global.css" })
+module.exports = withNativeWind(config, { input: "./app/global.css" })
