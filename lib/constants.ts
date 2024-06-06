b01a618f7834359d09a38ffcb66524ce78da2412
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-06-06 17:13:14 -0400

    updated the theme colors to match the css file

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,9 +8,9 @@ export const NAV_THEME = {
     text: "hsl(240 10% 3.9%)", // foreground
   },
   dark: {
-    background: "hsl(240 10% 3.9%)", // background
+    background: "hsl(203 2.47% 7.51%)", // background
     border: "hsl(240 3.7% 15.9%)", // border
-    card: "hsl(240 10% 3.9%)", // card
+    card: "hsl(203 2.47% 7.51%)", // card
     notification: "hsl(0 72% 51%)", // destructive
     primary: "hsl(0 0% 98%)", // primary
     text: "hsl(0 0% 98%)", // foreground
